cd402f37fe52ee41ad91605b3eca266a56253c68
Alabhya Jindal <alabhya10@gmail.com>
2022-01-13 21:40:06 +0530

    completed alphabet arrays

--- app.js
+++ app.js
@@ -20,14 +20,68 @@ function numbersFunc() {
 }
 
 function uppercaseFunc() {
-  const uppercaseList = ["A", "B", "C", "D", "E", "F"];
+  const uppercaseList = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
   password.push(
     uppercaseList[Math.floor(Math.random() * uppercaseList.length)]
   );
 }
 
 function lowercaseFunc() {
-  const lowercaseList = ["a", "b", "c", "d", "e", "f"];
+  const lowercaseList = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
   password.push(
     lowercaseList[Math.floor(Math.random() * lowercaseList.length)]
   );
